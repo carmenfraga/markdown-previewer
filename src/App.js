ad46de1ad4efd292597272044d17865b409df95c
Carmen Fraga <carmenfragad@gmail.com>
2022-08-23 19:52:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
 
 function App() {
+  const [text, setText] = React.useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <textarea id="editor" value={text} onChange={handleChange}></textarea>
+      <ReactMarkdown id="preview">{text}</ReactMarkdown>
     </div>
   );
 }
