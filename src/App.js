9b913776b9aaef1b4120d49522b0bed60b1260dd
Carmen Fraga <carmenfragad@gmail.com>
2022-08-23 20:35:37 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import "./App.css";
+import markdownText from "./markdown.md";
 
 function App() {
-  const [text, setText] = React.useState("");
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    fetch(markdownText)
+      .then((response) => response.text())
+      .then((responseText) => {
+        setText(responseText);
+      });
+  }, []);
 
   const handleChange = (e) => {
     setText(e.target.value);
   };
 
   return (
-    <div>
+    <div className="px-4 text-center">
+      <h1 className="mb-4">My Markdown Previewer</h1>
       <textarea id="editor" value={text} onChange={handleChange}></textarea>
       <ReactMarkdown id="preview">{text}</ReactMarkdown>
     </div>
